c19d64bf6ef7e1ec570e3448309dad9bd64ff51d
IceFreez3r <39794212+IceFreez3r@users.noreply.github.com>
2021-03-29 15:13:29 +0200

    [TESTS, FIX] Simulate Alignment Test fixed for new implementation

--- test/api/jstmap/simulate/simulate_alignment_test.cpp
+++ test/api/jstmap/simulate/simulate_alignment_test.cpp
@@ -9,13 +9,14 @@
 
 #include <filesystem>
 
+#include <seqan3/alignment/aligned_sequence/aligned_sequence_concept.hpp>
 #include <jstmap/simulate/load_reference.hpp>
 #include <jstmap/simulate/simulate_alignment.hpp>
 
 TEST(jstmap_simulate, simulate_alignment)
 {
     std::filesystem::path sequence_file{DATADIR"sim_reads_ref1x10.fa"};
-    jstmap::aligned_sequence_t reference = jstmap::load_reference(sequence_file);
+    jstmap::sequence_t reference = jstmap::load_reference(sequence_file);
     jstmap::alignment_t alignment = jstmap::simulate_alignment(reference, 0.39); // ceil(20*0.39) = 8 => 4 SNPs, 2 Inserts, 2 Deletes
     EXPECT_EQ(alignment.first.size(), 22u);
     EXPECT_EQ(alignment.second.size(), 22u);
@@ -26,13 +27,15 @@ TEST(jstmap_simulate, simulate_alignment)
     EXPECT_EQ(variants, 8u);
     alignment.first.erase(std::remove(alignment.first.begin(), alignment.first.end(), seqan3::gapped<seqan3::dna5>{}.assign_char('-')), alignment.first.end());
     jstmap::aligned_sequence_t stripped_reference = alignment.first;
-    EXPECT_EQ(reference, stripped_reference);
+    jstmap::aligned_sequence_t aligned;
+    seqan3::assign_unaligned(aligned, reference);
+    EXPECT_EQ(aligned, stripped_reference);
 }
 
 TEST(jstmap_simulate, simulate_alignment_error_rate_zero)
 {
     std::filesystem::path sequence_file{DATADIR"sim_reads_ref1x10.fa"};
-    jstmap::aligned_sequence_t reference = jstmap::load_reference(sequence_file);
+    jstmap::sequence_t reference = jstmap::load_reference(sequence_file);
     jstmap::alignment_t alignment = jstmap::simulate_alignment(reference, 0); // 20*0.4 = 8 => 4 SNPs, 2 Inserts, 2 Deletes
     EXPECT_EQ(alignment.first.size(), 20u);
     EXPECT_EQ(alignment.second.size(), 20u);
@@ -46,13 +49,13 @@ TEST(jstmap_simulate, simulate_alignment_error_rate_zero)
 TEST(jstmap_simulate, simulate_alignment_negative_error_rate)
 {
     std::filesystem::path sequence_file{DATADIR"sim_reads_ref1x10.fa"};
-    jstmap::aligned_sequence_t reference = jstmap::load_reference(sequence_file);
-    EXPECT_THROW(jstmap::alignment_t alignment = jstmap::simulate_alignment(reference, -1), std::runtime_error);
+    jstmap::sequence_t reference = jstmap::load_reference(sequence_file);
+    EXPECT_THROW(jstmap::simulate_alignment(reference, -1), std::runtime_error);
 }
 
 TEST(jstmap_simulate, simulate_alignment_too_high_error_rate)
 {
     std::filesystem::path sequence_file{DATADIR"sim_reads_ref1x10.fa"};
-    jstmap::aligned_sequence_t reference = jstmap::load_reference(sequence_file);
-    EXPECT_THROW(jstmap::alignment_t alignment = jstmap::simulate_alignment(reference, 2), std::runtime_error);
+    jstmap::sequence_t reference = jstmap::load_reference(sequence_file);
+    EXPECT_THROW(jstmap::simulate_alignment(reference, 2), std::runtime_error);
 }
